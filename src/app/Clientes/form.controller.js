0d10b0d4c1f775dec4ecac31d80af9bc2ab0b171
Eduardo de Farias Cunha <edu.farias8@hotmail.com>
2019-07-25 21:53:10 -0300

    Aula dia 25/07

--- src/app/Clientes/form.controller.js
+++ src/app/Clientes/form.controller.js
@@ -1,3 +1,27 @@
 export default class FormController{
-    
+    constructor($state, $stateParams, clienteService){
+        this.record = {}
+        this._$state = $state;
+        this._id = $stateParams.id;
+        this._clienteService = clienteService;
+        if (this._id) {
+            this.findById()
+        }
+    }
+    async save() {
+        if (this._id){
+            await this._clienteService.update(this.record)
+        }else{
+            await this._clienteService.insert(this.record)
+        }
+        this._$state.go("app.cliente.list");
+    }
+    findById() {
+        return this._clienteService.findById(this._id)
+        .then(response => {
+            return this.record;
+        })
+    }
 }
+
+FormController.$inject = ["$state", "$stateParams", "clienteService"]
